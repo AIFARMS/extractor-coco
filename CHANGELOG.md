661a5bd5b23407f2df2fed602fb074554077af58
Rob Kooper <kooper@illinois.edu>
2022-10-12 22:09:09 -0500

    color and multiple annotation and thumbnails
    - fixed bug that would only parse first coco file #3
    - fixed bug that would not parse hex colors #4
    - added thumbnail to file
    - added title to preview

---
+++ CHANGELOG.md
@@ -0,0 +1,26 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](http://semver.org/).
+
+
+## 1.0.2 - 2022-10-12
+
+## Added
+- Generate a thumbnail for each preview uploaded
+- Set title of the preview to coco
+
+## Fixed
+- Would only process first annotations found [#3](https://github.com/AIFARMS/extractor-coco/issues/3)
+- Could not process color in hex format [#4](https://github.com/AIFARMS/extractor-coco/issues/4)
+
+## 1.0.1 - 2022-09-20
+
+## Added
+- Add parameters to extractor to set linewidth and color
+
+## 1.0.0 -
+
+Initial release
--- extractor.py
+++ extractor.py
@@ -36,6 +36,42 @@ class CocoAnnotation(Extractor):
             return CheckMessage.download
         return CheckMessage.ignore
 
+    @staticmethod
+    def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+        # initialize the dimensions of the image to be resized and
+        # grab the image size
+        dim = None
+        (h, w) = image.shape[:2]
+
+        # if both the width and height are None, then return the
+        # original image
+        if width is None and height is None:
+            return image
+
+        # check to see if the width is None
+        if width is None:
+            # calculate the ratio of the height and construct the
+            # dimensions
+            r = height / float(h)
+            if r >= 1:
+                return image
+            dim = (int(w * r), height)
+
+        # otherwise, the height is None
+        else:
+            # calculate the ratio of the width and construct the
+            # dimensions
+            r = width / float(w)
+            if r >= 1:
+                return image
+            dim = (width, int(h * r))
+
+        # resize the image
+        resized = cv2.resize(image, dim, interpolation = inter)
+
+        # return the resized image
+        return resized
+
     # Overridden method that performs the processing on the message
     # Prepares the environment for Open Drone Map by linking to image files and
     # checking settings for overrides. Also uploads the results of the run
@@ -44,55 +80,75 @@ class CocoAnnotation(Extractor):
             user_params = {}
         else:
             user_params = json.loads(parameters["parameters"])
-        rgb = webcolors.name_to_rgb(user_params.get("color", "purple"))
+        color = user_params.get("color", "#8b008b")
+        try:
+            if color.startswith("#"):\
+                rgb = webcolors.hex_to_rgb(color)
+            else:
+                rgb = webcolors.name_to_rgb(color)
+        except ValueError:
+            rgb = webcolors.hex_to_rgb("#8b008b")
         width = int(user_params.get("width", "5"))
 
-        annotation_file = None
+        annotation_files = []
         for localfile in resource['local_paths']:
             if localfile.lower().endswith('.coco.json'):
-                annotation_file = localfile
-        self.logger.debug(f"Annotation file {annotation_file}")
-        if not annotation_file:
+                annotation_files.append(localfile)
+        self.logger.debug(f"Annotation file {','.join(annotation_files)}")
+        if not annotation_files:
             connector.status_update(StatusMessage.processing, resource, "Could not find coco.json file.")
             return
 
         # load the annotations
-        with open(annotation_file, "r") as jsonFile:
-            data = json.load(jsonFile)
+        for annotation_file in annotation_files:
+            with open(annotation_file, "r") as jsonFile:
+                data = json.load(jsonFile)
 
-        # add metadata to dataset
-        content = data.get('info', []).copy()
-        content['licenses'] = [x['name'] for x in data.get('licenses', []) if 'name' in x]
-        metadata = self.get_metadata(content, resource['type'], resource['id'], host)
-        pyclowder.datasets.upload_metadata(connector, host, secret_key, resource['id'], metadata)
+            # add metadata to dataset
+            content = data.get('info', []).copy()
+            content['filename'] = annotation_file
+            content['licenses'] = [x['name'] for x in data.get('licenses', []) if 'name' in x]
+            content['annotations'] = len(data["annotations"])
+            content['images'] = len(data["images"])
+            metadata = self.get_metadata(content, resource['type'], resource['id'], host)
+            pyclowder.datasets.upload_metadata(connector, host, secret_key, resource['id'], metadata)
 
-        # loop through all images
-        for image in data["images"]:
-            # find all annotations
-            annotations = [a for a in data["annotations"] if a['image_id'] == image['id']]
+            # loop through all images
+            for image in data["images"]:
+                # find all annotations
+                annotations = [a for a in data["annotations"] if a['image_id'] == image['id']]
 
-            # find image in clowder
-            clowder_image = next((x for x in resource['files'] if x['filename'] == image['file_name']), None)
-            local_image = next((x for x in resource['local_paths'] if x.endswith(image['file_name'])), None)
-            if annotations and clowder_image and local_image:
-                img = cv2.imread(local_image)
+                # find image in clowder
+                clowder_image = next((x for x in resource['files'] if x['filename'] == image['file_name']), None)
+                local_image = next((x for x in resource['local_paths'] if x.endswith(image['file_name'])), None)
+                if annotations and clowder_image and local_image:
+                    img = cv2.imread(local_image)
 
-                for a in annotations:
-                    x = int(a['bbox'][0])
-                    y = int(a['bbox'][1])
-                    w = int(a['bbox'][2])
-                    h = int(a['bbox'][3])
-                    #add a rectangle to the image based on the bounding box coordinates
-                    img = cv2.rectangle(img,(x,y),(x+w,y+h), rgb, width)
-                    #write the images with annotations overlaid to the created folder
+                    for a in annotations:
+                        x = int(a['bbox'][0])
+                        y = int(a['bbox'][1])
+                        w = int(a['bbox'][2])
+                        h = int(a['bbox'][3])
+                        #add a rectangle to the image based on the bounding box coordinates
+                        img = cv2.rectangle(img,(x,y),(x+w,y+h), rgb, width)
 
-                handle, outfile = tempfile.mkstemp(suffix=".jpg")
-                try:
-                    os.close(handle)
-                    cv2.imwrite(outfile, img)
-                    pyclowder.files.upload_preview(connector, host, secret_key, clowder_image['id'], outfile)
-                finally:
-                    os.unlink(outfile)
+                    # write preview
+                    handle, outfile = tempfile.mkstemp(suffix=".jpg")
+                    try:
+                        os.close(handle)
+
+                        cv2.imwrite(outfile, self.image_resize(img, width=800))
+                        previewid = pyclowder.files.upload_preview(connector, host, secret_key, clowder_image['id'], outfile)
+
+                        url = '%sapi/previews/%s/title?key=%s' % (host, previewid, secret_key)
+                        headers = {'Content-Type': 'application/json'}
+                        connector.post(url, headers=headers, data=json.dumps({"title": "coco"}),
+                                       verify=connector.ssl_verify if connector else True)
+
+                        cv2.imwrite(outfile, self.image_resize(img, width=225))
+                        pyclowder.files.upload_thumbnail(connector, host, secret_key, clowder_image['id'], outfile)
+                    finally:
+                        os.unlink(outfile)
 
 
 if __name__ == "__main__":
